5895cc97fbcfc68d6c88a37217b147e0ca9e68a2
Julien Capellari <julien.capellari@gmail.com>
2023-11-21 19:40:18 +0100

    feat: add ref concept

---
+++ src/defs/concepts/ref.ts
@@ -0,0 +1,22 @@
+import { Source } from './source.js';
+import { AsyncReadable, MapReadValue, Readable, SyncReadable } from '../features/readable.js';
+
+/**
+ * Readonly reference
+ */
+export type Ref<D = unknown, R extends Readable<D> = Readable<D>> = Source<D> & R;
+
+/**
+ * Readonly synchronous reference
+ */
+export type SyncRef<D = unknown> = Ref<D, SyncReadable<D>>;
+
+/**
+ * Readonly asynchronous reference
+ */
+export type AsyncRef<D = unknown> = Ref<D, AsyncReadable<D>>;
+
+/**
+ * Build a Ref type with the same synchronicity and the given value type
+ */
+export type MapRefValue<R extends Readable, D> = Ref<D, MapReadValue<R, D>>;
