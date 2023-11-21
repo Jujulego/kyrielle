18e4d990b32f77a9c78e671a6923c18b9f12e8fb
Julien Capellari <julien.capellari@gmail.com>
2023-11-21 12:23:21 +0100

    chore: add merge$ operator

---
+++ tests/operators/merge.test-d.ts
@@ -0,0 +1,15 @@
+import { expectTypeOf } from 'vitest';
+
+import { merge$ } from '@/src/operators/merge.js';
+import { source$ } from '@/src/source.js';
+
+// Tests
+describe('merge$', () => {
+  it('should emit union of input types', () => {
+    const int = source$<number>();
+    const str = source$<string>();
+    const src = merge$(int, str);
+
+    expectTypeOf(src.subscribe).parameter(0).parameter(0).toEqualTypeOf<number | string>();
+  });
+});
---
+++ tests/operators/merge.test.ts
@@ -0,0 +1,52 @@
+import { vi } from 'vitest';
+
+import { Listener } from '@/src/defs/common.js';
+import { merge$ } from '@/src/operators/merge.js';
+import { source$ } from '@/src/source.js';
+
+// Tests
+describe('merge$', () => {
+  it('should subscribe to each sources', () => {
+    const int = source$<number>();
+    vi.spyOn(int, 'subscribe');
+
+    const str = source$<string>();
+    vi.spyOn(str, 'subscribe');
+
+    const listener: Listener<number | string> = vi.fn();
+    const src = merge$(int, str);
+    src.subscribe(listener);
+
+    expect(int.subscribe).toHaveBeenCalledWith(listener);
+    expect(str.subscribe).toHaveBeenCalledWith(listener);
+  });
+
+  it('should unsubscribe from each sources', () => {
+    const int = source$<number>();
+    vi.spyOn(int, 'unsubscribe');
+
+    const str = source$<string>();
+    vi.spyOn(str, 'unsubscribe');
+
+    const listener: Listener<number | string> = vi.fn();
+    const src = merge$(int, str);
+    src.unsubscribe(listener);
+
+    expect(int.unsubscribe).toHaveBeenCalledWith(listener);
+    expect(str.unsubscribe).toHaveBeenCalledWith(listener);
+  });
+
+  it('should clear each sources', () => {
+    const int = source$<number>();
+    vi.spyOn(int, 'clear');
+
+    const str = source$<string>();
+    vi.spyOn(str, 'clear');
+
+    const src = merge$(int, str);
+    src.clear();
+
+    expect(int.clear).toHaveBeenCalledWith();
+    expect(str.clear).toHaveBeenCalledWith();
+  });
+});
