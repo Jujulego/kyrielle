5fcb257aaf8bfb47b37f9726d065a6070c77fb99
Julien Capellari <julien.capellari@gmail.com>
2023-11-21 19:18:42 +0100

    Add filter step (#21)
    * chore: add filter step
    
    * fix: tests errors

---
+++ tests/steps/filter.test-d.ts
@@ -0,0 +1,19 @@
+import { describe, expectTypeOf } from 'vitest';
+
+import { pipe$ } from '@/src/operators/pipe.js';
+import { filter$ } from '@/src/steps/filter.js';
+import { source$ } from '@/src/source.js';
+
+// Tests
+describe('filter$', () => {
+  it('should use type from predicate', () => {
+    const ref = pipe$(
+      source$<number | string>(),
+      filter$((arg): arg is number => typeof arg === 'number')
+    );
+
+    expectTypeOf(ref.next).parameter(0).toBeNumber();
+    expectTypeOf(ref).not.toHaveProperty('read');
+    expectTypeOf(ref).not.toHaveProperty('mutate');
+  });
+});
