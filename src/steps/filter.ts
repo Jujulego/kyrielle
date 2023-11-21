2e099711fc6aa42f06029d2cabd77fc08166d8c0
Julien Capellari <julien.capellari@gmail.com>
2023-11-21 19:13:06 +0100

    chore: add filter step

---
+++ src/steps/filter.ts
@@ -0,0 +1,22 @@
+import { Observable as Obs, Source } from '../defs/index.js';
+import { PipeStep } from '../operators/index.js';
+import { source$ } from '../source.js';
+
+// Operator
+export function filter$<DA, DB extends DA>(fn: (arg: DA) => arg is DB): PipeStep<Obs<DA>, Source<DB>>;
+
+export function filter$<D>(fn: (arg: D) => boolean): PipeStep<Obs<D>, Source<D>>;
+
+export function filter$<D>(fn: (arg: D) => boolean): PipeStep<Obs<D>, Source<D>> {
+  return (obs: Obs<D>, { off }) => {
+    const out = source$<D>();
+
+    off.add(obs.subscribe((data) => {
+      if (fn(data)) {
+        out.next(data);
+      }
+    }));
+
+    return out;
+  };
+}
