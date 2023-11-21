b344b4fa401c8c67bbb25b16295ff8b91c60138a
Julien Capellari <julien.capellari@gmail.com>
2023-11-22 00:17:15 +0100

    chore: add module exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 export * from './defs/index.js';
-
 export * from './source.js';
+
+export * as events from './events/index.js';
+export * as operators from './operators/index.js';
+export * as refs from './refs/index.js';
+export * as steps from './steps/index.js';
+export * as subscriptions from './subscriptions/index.js';
