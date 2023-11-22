8af12fcde315252337ada51d0674833c6e619916
Julien Capellari <julien.capellari@gmail.com>
2023-11-22 23:02:02 +0100

    chore: add collections module

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from './defs/index.js';
 export * from './source.js';
 
+export * as collections from './collections/index.js';
 export * as events from './events/index.js';
 export * as operators from './operators/index.js';
 export * as refs from './refs/index.js';
