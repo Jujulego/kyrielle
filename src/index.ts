e07cabda9fd7e6e06552d3b0da988d88c79eb4a0
Julien Capellari <julien.capellari@gmail.com>
2024-02-14 18:38:47 +0100

    feat: add pipe$

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from './defs/index.js';
 export * from './mutable$.js';
 export * from './observable$.js';
+export * from './pipe$.js';
 export * from './readable$.js';
 export * from './resource-builder$.js';
 export * from './source$.js';
