4da4d31314e6515370785431a9646e7c88af4dfa
Julien Capellari <julien.capellari@gmail.com>
2024-03-10 23:18:01 +0100

    feat: test const$ and var$

--- src/var$.ts
+++ src/var$.ts
@@ -2,7 +2,7 @@ import { Mutable, Observable, Observer, Readable, SubscribeCallbacks, Subscripti
 import { parseSubscribeArgs } from './utils/subscribe.js';
 import { buildSubscription } from './utils/subscription.js';
 
-export interface Var<in out D> extends Observable<D>, Readable<D>, Mutable<D> {}
+export interface Var<in out D> extends Observable<Exclude<D, undefined>>, Readable<D>, Mutable<D> {}
 
 /**
  * Builds an uninitialized var
@@ -16,14 +16,24 @@ export function var$<D>(): Var<D | undefined>;
 export function var$<D>(initial: D): Var<D>;
 
 export function var$<D>(initial?: D): Var<D | undefined> {
-  const observers = new Set<Observer<D | undefined>>();
+  const observers = new Set<Observer<Exclude<D, undefined>>>();
   let value = initial;
 
   const _var = {
     read: () => value,
-    mutate: (arg: D | undefined) => value = arg,
+    mutate: (arg: D | undefined) => {
+      value = arg;
+
+      if (value !== undefined) {
+        for (const observer of observers) {
+          observer.next(value as Exclude<D, undefined>);
+        }
+      }
+
+      return value;
+    },
     [Symbol.observable ?? '@@observable']: () => _var,
-    subscribe(...args: [Partial<Observer<D | undefined>>] | SubscribeCallbacks<D | undefined>): Subscription {
+    subscribe(...args: [Partial<Observer<Exclude<D, undefined>>>] | SubscribeCallbacks<Exclude<D, undefined>>): Subscription {
       const observer = parseSubscribeArgs(args);
       const subscription = buildSubscription({
         onUnsubscribe: () => observers.delete(observer),
@@ -32,7 +42,10 @@ export function var$<D>(initial?: D): Var<D | undefined> {
 
       observers.add(observer);
       observer.start?.(subscription);
-      observer.next(value);
+
+      if (value !== undefined) {
+        observer.next(value as Exclude<D, undefined>);
+      }
 
       return subscription;
     },
