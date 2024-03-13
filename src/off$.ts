64028cf25c6e27cf300436dcce8da1647b3437ed
Julien Capellari <julien.capellari@gmail.com>
2024-03-13 23:10:54 +0100

    feat: add off$ utility

---
+++ src/off$.ts
@@ -0,0 +1,36 @@
+import { Unsubscribable } from './defs/index.js';
+
+/**
+ * Represents a subscription group
+ */
+export interface SubscriptionGroup extends Unsubscribable, Disposable {
+  /**
+   * Add given subscription to the group
+   */
+  add(...subscriptions: Unsubscribable[]): void;
+}
+
+/**
+ * Builds a subscription group
+ */
+export function off$(...subscriptions: Unsubscribable[]): SubscriptionGroup {
+  const store = new Set(subscriptions);
+
+  function unsubscribe() {
+    for (const sub of store) {
+      sub.unsubscribe();
+    }
+
+    store.clear();
+  }
+
+  return {
+    [Symbol.dispose ?? Symbol.for('Symbol.dispose')]: unsubscribe,
+    unsubscribe,
+    add: (...subscriptions: Unsubscribable[]) => {
+      for (const sub of subscriptions) {
+        store.add(sub);
+      }
+    }
+  };
+}
