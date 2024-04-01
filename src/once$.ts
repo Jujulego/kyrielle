2d5c33182ade864cb5669cf854872a5a353142be
Julien Capellari <julien.capellari@gmail.com>
2024-04-01 23:42:43 +0200

    feat: add once & test it

---
+++ src/once$.ts
@@ -0,0 +1,45 @@
+import type {
+  Listenable,
+  ListenEventMap,
+  MappingKey, Observable,
+  ObservedValue,
+  Observer,
+  Subscribable,
+  Unsubscribable
+} from './defs/index.js';
+
+/**
+ * Register an observer that completes right after the first call (either "next" or "error")
+ */
+export function once$<O extends Subscribable>(observable: O, observer: Partial<Observer<ObservedValue<O>>>): ReturnType<O['subscribe']>;
+export function once$<L extends Listenable, K extends MappingKey<ListenEventMap<L>>>(listenable: L, key: K, observer: Partial<Observer<ListenEventMap<L>[K]>>): ReturnType<L['on']>;
+export function once$(...args: [Observable, Partial<Observer>] | [Listenable, string, Partial<Observer>]): Unsubscribable {
+  let sub: Unsubscribable;
+
+  if (typeof args[1] === 'string') {
+    const [listener, key, observer] = args as [Listenable, string, Partial<Observer>];
+    sub = listener.on(key, prepareObserver(observer, () => sub.unsubscribe()));
+  } else {
+    const [observable, observer] = args as [Observable, Partial<Observer>];
+    sub = observable.subscribe(prepareObserver(observer, () => sub.unsubscribe()));
+  }
+
+  return sub;
+}
+
+function prepareObserver(base: Partial<Observer>, unsub: () => void): Partial<Observer> {
+  const result: Partial<Observer> = {};
+
+  for (const cb of ['next', 'error', 'complete'] as const) {
+    if (cb in base) {
+      Object.assign(result, {
+        [cb]: (...args: [arg?: unknown]) => {
+          unsub();
+          base[cb]!(...args);
+        }
+      });
+    }
+  }
+
+  return result;
+}
