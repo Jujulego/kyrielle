aee423649c681ee60ba9cf740e5af7eb837b1cf2
Julien Capellari <julien.capellari@gmail.com>
2024-04-13 23:27:10 +0200

    feat: add merge implementation

---
+++ src/merge$.ts
@@ -0,0 +1,38 @@
+import type { Observable, Subscribable, Unsubscribable } from './defs/index.js';
+
+import { observable$ } from './observable$.js';
+
+export function merge$<D>(...observables: Subscribable<D>[]): Observable<D> {
+  return observable$((observer, signal) => {
+    const subscriptions = new Set<Unsubscribable>();
+
+    function handleAbort() {
+      for (const sub of subscriptions) {
+        sub.unsubscribe();
+      }
+    }
+
+    for (const obs of observables) {
+      let subscription: Unsubscribable;
+
+      obs.subscribe({
+        start(sub) {
+          subscriptions.add(sub);
+          subscription = sub;
+        },
+        next: (data) => observer.next(data),
+        error: (err) => observer.error(err),
+        complete() {
+          subscriptions.delete(subscription);
+
+          if (subscriptions.size === 0) {
+            signal.removeEventListener('abort', handleAbort);
+            observer.complete();
+          }
+        }
+      });
+    }
+
+    signal.addEventListener('abort', handleAbort, { once: true });
+  });
+}
