b4122b02da370da72be7e5b94cf404aaae7e2553
Julien Capellari <julien.capellari@gmail.com>
2024-09-21 15:55:07 +0200

    chore: fix eslint error

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,33 @@
 // @ts-check
 import eslint from '@eslint/js';
 import vitest from 'eslint-plugin-vitest';
-import { dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
 import tsEslint from 'typescript-eslint';
+
 
 export default tsEslint.config(
   {
     ignores: ['.pnp.*', '.yarn', 'coverage', 'dist']
   },
   eslint.configs.recommended,
-  ...tsEslint.configs.recommendedTypeChecked,
+  ...tsEslint.configs.recommendedTypeChecked.map((cfg) => ({ ...cfg, files: ['**/*.{ts,tsx}'] })),
   {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    rules: {
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+      'no-console': ['error', {
+        allow: ['warn', 'error'],
+      }],
+    }
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
     languageOptions: {
       parserOptions: {
         project: true,
-        tsconfigRootDir: dirname(fileURLToPath(import.meta.url)),
+        tsconfigRootDir: import.meta.dirname,
       },
     },
-  },
-  {
-    files: ['**/*.{js,jsx,ts,tsx}'],
     rules: {
       '@typescript-eslint/consistent-type-imports': 'error',
       '@typescript-eslint/no-empty-object-type': ['error', {
@@ -45,12 +52,22 @@ export default tsEslint.config(
     },
     rules: {
       ...vitest.configs.recommended.rules,
+    }
+  },
+  {
+    files: ['**/*.test.{ts,tsx}', '**/*.test-d.{ts,tsx}'],
+    rules: {
       '@typescript-eslint/no-unused-vars': ['off'],
       '@typescript-eslint/prefer-promise-reject-errors': ['off'],
       '@typescript-eslint/require-await': ['off'],
       '@typescript-eslint/unbound-method': ['off'],
+    }
+  },
+  {
+    files: ['**/*.test-d.{ts,tsx}'],
+    rules: {
       'vitest/expect-expect': ['error', {
-        assertFunctionNames: ['expect', 'expectTypeOf', 'assertType']
+        assertFunctionNames: ['expectTypeOf', 'assertType']
       }],
     }
   }
