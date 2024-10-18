23cad29b19227ab93292a3180b21c343aa96764e
Julien Capellari <julien.capellari@gmail.com>
2024-10-18 23:40:07 +0200

    feat: add Iterable definition

---
+++ src/types/inputs/Iterable.ts
@@ -0,0 +1,24 @@
+/**
+ * Object that can be iterated
+ *
+ * @since 2.0.0
+ */
+export interface Iterable<out D> {
+  /**
+   * Returns next item.
+   *
+   * @since 2.0.0
+   */
+  next(): IterableResult<D>;
+}
+
+export interface IterableResultValue<out D> {
+  readonly value: D;
+  readonly done?: false;
+}
+
+export interface IterableResultDone {
+  readonly done: true;
+}
+
+export type IterableResult<D> = IterableResultValue<D> | IterableResultDone;
