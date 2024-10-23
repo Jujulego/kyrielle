dece59e18d785ec0c0a8d84d8b8de9eee8399994
Julien Capellari <julien.capellari@gmail.com>
2024-10-23 23:39:09 +0200

    feat: add Iterator support to json$

--- src/json$.ts
+++ src/json$.ts
@@ -1,18 +1,31 @@
+import { iterator$ } from './iterator$.js';
 import { observable$ } from './observable$.js';
 import { resource$ } from './resource$.js';
 import type { AsyncDeferrable, Deferrable } from './types/inputs/Deferrable.js';
+import type { AnyIterable } from './types/inputs/Iterable.js';
 import type { AsyncMutable, Mutable } from './types/inputs/Mutable.js';
 import type { AnySubscribable } from './types/inputs/Subscribable.js';
 import type { AsyncMutator, Mutator } from './types/outputs/Mutator.js';
 import type { Observable } from './types/outputs/Observable.js';
 import type { AsyncRef, Ref } from './types/outputs/Ref.js';
+import type { SimpleIterator } from './types/outputs/SimpleIterator.js';
 import type { Awaitable } from './types/utils.js';
 import { applyFn } from './utils/fn.js';
-import { isDeferrable, isMutable, isSubscribable } from './utils/predicates.js';
+import { extractIterator } from './utils/iterator.js';
+import {
+  isDeferrable,
+  isIterable,
+  isMinimalIterator,
+  isMutable,
+  isSubscribable,
+  isSubscribableHolder
+} from './utils/predicates.js';
+import { extractSubscribable } from './utils/subscribable.js';
 import { boundedSubscription } from './utils/subscription.js';
 
 // Types
 export type JsonOrigin =
+  | AnyIterable<string>
   | AnySubscribable<string>
   | Deferrable<Awaitable<string>>
   | Mutable<string, Awaitable<string>>;
@@ -21,6 +34,7 @@ export type JsonMutable<O extends Mutable, D, A> = O extends AsyncMutable ? Asyn
 export type JsonDeferrable<O extends Deferrable, D> = O extends AsyncDeferrable ? AsyncRef<D> : Ref<D>;
 
 export type JsonResult<O, D, A> =
+  & (O extends AnyIterable ? SimpleIterator<D> : unknown)
   & (O extends AnySubscribable ? Observable<D> : unknown)
   & (O extends Deferrable ? JsonDeferrable<O, D> : unknown)
   & (O extends Mutable ? JsonMutable<O, D, A> : unknown);
@@ -57,10 +71,24 @@ export function json$() {
     // Builder
     const builder = resource$();
 
-    if (isSubscribable<string>(origin)) {
+    if (isIterable<string>(origin) || isMinimalIterator<string>(origin)) {
+      const iterator = extractIterator(origin);
+      builder.add(iterator$({
+        next: () => {
+          const next = iterator.next();
+
+          return next.done
+            ? { done: true }
+            : { done: false, value: parser(next.value) };
+        }
+      }));
+    }
+
+    if (isSubscribable<string>(origin) || isSubscribableHolder<string>(origin)) {
+      const observable = extractSubscribable(origin);
       builder.add(observable$((observer, signal) => {
-        boundedSubscription(origin, signal, {
-          next(val) {
+        boundedSubscription(observable, signal, {
+          next: (val) => {
             observer.next(parser(val));
           },
           error: (err) => observer.error(err),
