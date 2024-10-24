0280b5746d848151c2d85ad2aeb9dc460b90cfc4
Julien Capellari <julien.capellari@gmail.com>
2024-10-25 00:06:40 +0200

    feat: rework scan$

--- tests/scan$.test.ts
+++ tests/scan$.test.ts
@@ -5,17 +5,14 @@ import { describe, expect, it, vi } from 'vitest';
 
 // Tests
 describe('scan$', () => {
-  it('should emit each result', async () => {
-    const spyInit = vi.fn(() => 0);
+  it('should call compute on each emitted value', () => {
     const spyCompute = vi.fn((state: number, item: number) => state + item);
     const spyResult = vi.fn();
 
     pipe$(
       of$([1, 2, 3]),
-      scan$(spyCompute, spyInit),
+      scan$(spyCompute, 0),
     ).subscribe(spyResult);
-
-    expect(spyInit).toHaveBeenCalledOnce();
 
     expect(spyCompute).toHaveBeenCalledWith(0, 1);
     expect(spyResult).toHaveBeenCalledWith(1);
@@ -25,5 +22,25 @@ describe('scan$', () => {
 
     expect(spyCompute).toHaveBeenCalledWith(3, 3);
     expect(spyResult).toHaveBeenCalledWith(6);
+  });
+
+  it('should call compute on each iterated value', () => {
+    const spyCompute = vi.fn((state: number, item: number) => state + item);
+
+    const iterator = pipe$(
+      [1, 2, 3],
+      scan$(spyCompute, 0),
+    );
+
+    expect(iterator.next()).toStrictEqual({ done: false, value: 1 });
+    expect(spyCompute).toHaveBeenCalledWith(0, 1);
+
+    expect(iterator.next()).toStrictEqual({ done: false, value: 3 });
+    expect(spyCompute).toHaveBeenCalledWith(1, 2);
+
+    expect(iterator.next()).toStrictEqual({ done: false, value: 6 });
+    expect(spyCompute).toHaveBeenCalledWith(3, 3);
+
+    expect(iterator.next()).toStrictEqual({ done: true });
   });
 });
