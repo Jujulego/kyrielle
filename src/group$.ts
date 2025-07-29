d47b4a3335c4e43c986cf391c9cbed47a8b34d72
Julien Capellari <julien.capellari@gmail.com>
2025-07-30 00:28:18 +0200

    feat: improve group$ documentation

--- src/group$.ts
+++ src/group$.ts
@@ -1,11 +1,13 @@
 import { _multiplexer, type _Multiplexer } from './bases/_multiplexer.js';
-import type { Subscribable, SubscribableValue } from './types/inputs/Subscribable.js';
 import { merge$ } from './merge$.js';
+import type { Subscribable, SubscribableValue } from './types/inputs/Subscribable.js';
 import type { Observable } from './types/outputs/Observable.js';
 
 /**
  * Builds a multiplexer routing events to origins within the given map, and an observable that emit every
  * value emitted by the given origins.
+ *
+ * @since 2.3.0
  */
 export function group$<M extends GroupMapping>(origins: M): Group<M> {
   return Object.assign(
