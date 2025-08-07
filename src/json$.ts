893a70c997a5ab5869e5cf3d240587bd07c2910c
Julien Capellari <julien.capellari@gmail.com>
2025-08-08 01:12:19 +0200

    feat: add support for async iterators to json$

--- src/json$.ts
+++ src/json$.ts
@@ -1,22 +1,31 @@
+import { asyncIterator$ } from './async-iterator$.js';
 import { iterator$ } from './iterator$.js';
 import { observable$ } from './observable$.js';
 import { resource$ } from './resource$.js';
 import type { AsyncDeferrable, Deferrable } from './types/inputs/Deferrable.js';
-import type { AnyIterable } from './types/inputs/MinimalIterator.js';
+import type {
+  AnyAsyncIterable,
+  AnyAwaitableIterable,
+  AnyIterable,
+  MinimalAsyncIterator,
+  MinimalIterator
+} from './types/inputs/MinimalIterator.js';
 import type { AsyncMutable, Mutable } from './types/inputs/Mutable.js';
 import type { AnySubscribable } from './types/inputs/Subscribable.js';
 import type { AsyncMutator, Mutator } from './types/outputs/Mutator.js';
 import type { Observable } from './types/outputs/Observable.js';
 import type { AsyncRef, Ref } from './types/outputs/Ref.js';
-import type { SimpleIterator } from './types/outputs/SimpleIterator.js';
+import type { SimpleAsyncIterator, SimpleIterator } from './types/outputs/SimpleIterator.js';
 import type { Awaitable } from './types/utils.js';
 import { applyFn } from './utils/fn.js';
-import { extractIterator } from './utils/iterator.js';
+import { extractAwaitableIterator } from './utils/iterator.js';
 import {
+  isAsyncIterable,
+  isAwaitableIterator,
   isDeferrable,
   isIterable,
-  isMinimalIterator,
   isMutable,
+  isPromise,
   isSubscribable,
   isSubscribableHolder
 } from './utils/predicates.js';
@@ -25,7 +34,7 @@ import { boundedSubscription } from './utils/subscription.js';
 
 // Types
 export type JsonOrigin =
-  | AnyIterable<string>
+  | AnyAwaitableIterable<string>
   | AnySubscribable<string>
   | Deferrable<Awaitable<string>>
   | Mutable<string, Awaitable<string>>;
@@ -34,7 +43,11 @@ export type JsonMutable<O extends Mutable, D, A> = O extends AsyncMutable ? Asyn
 export type JsonDeferrable<O extends Deferrable, D> = O extends AsyncDeferrable ? AsyncRef<D> : Ref<D>;
 
 export type JsonResult<O, D, A> =
-  & (O extends AnyIterable ? SimpleIterator<D> : unknown)
+  & (O extends AnyIterable
+    ? SimpleIterator<D>
+    : O extends AnyAsyncIterable
+      ? SimpleAsyncIterator<D>
+      : unknown)
   & (O extends AnySubscribable ? Observable<D> : unknown)
   & (O extends Deferrable ? JsonDeferrable<O, D> : unknown)
   & (O extends Mutable ? JsonMutable<O, D, A> : unknown);
@@ -71,17 +84,31 @@ export function json$() {
     // Builder
     const builder = resource$();
 
-    if (isIterable<string>(origin) || isMinimalIterator<string>(origin)) {
-      const iterator = extractIterator(origin);
-      builder.add(iterator$({
-        next: () => {
-          const next = iterator.next();
+    if (isIterable<string>(origin) || isAsyncIterable<string>(origin) || isAwaitableIterator<string>(origin)) {
+      const iterator = extractAwaitableIterator(origin);
+      const result = iterator.next();
 
-          return next.done
-            ? { done: true }
-            : { done: false, value: parser(next.value) };
-        }
-      }));
+      if (isPromise(result)) {
+        builder.add(asyncIterator$<unknown>((async function* () {
+          let res = await result;
+
+          while (!res.done) {
+            yield parser(res.value);
+
+            res = await (iterator as MinimalAsyncIterator<string>).next();
+          }
+        })()));
+      } else {
+        let res = result;
+
+        builder.add(iterator$<unknown>((function* () {
+          while (!res.done) {
+            yield parser(res.value);
+
+            res = (iterator as MinimalIterator<string>).next();
+          }
+        })()));
+      }
     }
 
     if (isSubscribable<string>(origin) || isSubscribableHolder<string>(origin)) {
